cf0b9ef36a0e0191dc911437e03f4990bbdeceb5
rumer <rumerenny@yahoo.com>
2024-12-24 12:25:22 -0700

    first project commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def print_my_name():
+    name = "Rume"
+    print(f"My name is {name}") # string interpolation or format string
+
+if __name__ == "__main__":
+    print_my_name()
+
+    # A repository is a project. There are different branches (features) in a repository
+    # Developers commit their work into branches and from there it is pushed into github (cloud)
+    # Github merges (merging) all the branches together and a piece of working software is created
+    #  In git there is a master branch called main
